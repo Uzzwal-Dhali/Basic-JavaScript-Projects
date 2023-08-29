a13c1719afe18388ff96eb77b386e9c77fcac925
Uzzwal Dhali <d.uzzwal09@gmail.com>
2023-08-29 11:06:37 +0600

    Copy buuton added!

--- Generate Hex Color Codes/main.js
+++ Generate Hex Color Codes/main.js
@@ -6,11 +6,17 @@ function main() {
   const button = document.querySelector(".button");
   const container = document.querySelector(".container");
   const display = document.getElementById("display");
+  const copy = document.getElementById("copy");
 
   button.addEventListener("click", function() {
     const bgColor = generateColor();
     container.style.background = bgColor;
     display.value = bgColor;
+  });
+
+  copy.addEventListener('click', function() {
+    navigator.clipboard.writeText(display.value);
+    this.innerHTML = "Coppied";
   });
 }
 
